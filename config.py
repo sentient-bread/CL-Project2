8ca24812419d9e7f489b3c9583c513036eb9e6bb
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-07-24 13:08:00 +0530

    Generalise seed, add parser

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-SEED_URL = "http://premchand.co.in/story/pariksha"
+from __future__ import unicode_literals
+from isc_parser import Parser
+
+SEED_URL = "http://premchand.co.in/story/"
 SENTENCE_DELIMITER = r"(!|\?|।)"
+
+parser = Parser(lang='hin')
